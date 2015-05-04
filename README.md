eaedfc382b146ca6fca0046393299aa4e8891cba
Cropse <cropse0219@gmail.com>
2015-05-04 15:29:16 +0800

    cleanup CODE and add introduce

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# sublime-Keil-C
-Compile keil C with sublime project
+# Sublime Keil Compiler
+This is Sublime Text Plugin for embedded developer with Keil C
+Sublime project must upon or beside the keil path
+Compiling log file will jump up when the Compilier is done
+click Tools->keil Compilier
+
+這是我自己做的一套sublime Text plugin
+因為自己平常不會使用keil來編輯,所以寫了一套簡單的編譯器方便在sublime Text上直接編譯
+只要sublime跟keil在同一個文件底下就可以直接編譯
+編譯完之後就會跳出keil的執行結果
+如果有任何意見或是建議歡迎提出
+
--- keil C complie.py
+++ keil C complie.py
@@ -2,16 +2,11 @@ import os
 import sublime, sublime_plugin
 import subprocess , os
 
-class ComplieCommand(sublime_plugin.TextCommand):
+class CompileCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
-		#self.view.insert(edit, 0, "Hello, World!")
-		
 
 		k=self.view.window().extract_variables()
-		#print (k)
-		#print (k["project_path"])
-		os.chdir(k["project_path"])
-								
+		os.chdir(k["project_path"])							
 		path=os.listdir(os.getcwd())
 						
 		for i in path:
@@ -19,19 +14,11 @@ class ComplieCommand(sublime_plugin.TextCommand):
         		Project=i
 
 		Command="UV4 -b {0} -o log.txt".format(Project)
-						#print Command
+		
 		subprocess.call(Command,stdout=subprocess.PIPE,shell =False )
 
-
 		self.view.window().open_file(k["project_path"]+"/log.txt")
-		filea = open("log.txt","r")
-		print (filea.read())
-		#os.popen(Command)
-						
-		#allcontent = sublime.Region(0, self.view.size())
-		#print (allcontent)
-		#self.view.replace(edit, allcontent, 'Hello, world!')
-		#print (self.view.substr(allcontent))
-
+		#filea = open("log.txt","r")
+		#print (filea.read())
 print ("saved")
 
